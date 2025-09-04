adfaec7ee5f6f3c2ce25c375f05ce044799208db
Romain Mrad <romain.mrad@gmail.com>
2025-09-04 11:19:21 -0400

    Changing log level formatting

--- rotating_logger/src/rotatinglogger.py
+++ rotating_logger/src/rotatinglogger.py
@@ -15,25 +15,35 @@ class RotatingLogger(object):
         - ``error()``: log at ERROR level
         - ``critical()``: log at CRITICAL level
     """
+
     def __init__(
-            self,
-            backup_count: int = 30,
-            encoding: str = 'utf-8',
+        self,
+        backup_count: int = 30,
+        encoding: str = "utf-8",
     ):
         """
-        Constructor
+        Logger singleton class implementing the logging mechanism:
+            - ``debug()``: log at DEBUG level
+            - ``info()``: log at INFO level
+            - ``warning()``: log at WARNING level
+            - ``error()``: log at ERROR level
+            - ``critical()``: log at CRITICAL level
         """
         # Instantiate a logger and set its level to DEBUG
         self._logger = logging.getLogger(__name__)
         self._logger.setLevel(logging.DEBUG)
         # Format log messages: YYYY-MM-DD hh:mm:ss,sss - [Level] - Msg
-        self._formatter = logging.Formatter('%(asctime)s - [%(levelname)s] - %(message)s')
+        self._formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)-8s - %(message)s"
+        )
         # Checking if logs folder exists and creating it if not
-        log_folder = 'logs'
+        log_folder = "logs"
         if not os.path.exists(log_folder):
             os.makedirs(log_folder)
         # Creating path to log file: 'YYYY-MM-DD.log'
-        log_file = os.path.join(log_folder, datetime.now().strftime('%Y-%m-%d.log'))
+        log_file = os.path.join(
+            log_folder, datetime.now().strftime("%Y-%m-%d.log")
+        )
         # Checking if logger has handlers and adding one if not
         if not self._logger.hasHandlers():
             # Instantiating rotating file handler
@@ -41,7 +51,7 @@ class RotatingLogger(object):
                 filename=log_file,
                 maxBytes=1000000,
                 backupCount=backup_count,
-                encoding=encoding
+                encoding=encoding,
             )
             # Adding formatter to file handler, and adding file handler to logger
             self._file_handler.setFormatter(self._formatter)
